d7dc6f9cef8c7e5f5f8997780c3f94b4af16824a
Muhammad Azmat Kamal Khan <azmat.kamalkhan@gmail.com>
2022-09-17 11:05:26 +0200

    database config

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'test';
+$db['default']['password'] = 'azykamal';
+$db['default']['database'] = 'leads_database';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
